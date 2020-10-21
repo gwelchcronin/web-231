9079e12ac1c8c41c25c063425c80c2eb958879df
Georgia Welch-Cronin <gwelchcronin@bellevue.edu>
2020-10-21 13:45:12 -0500

    Add files via upload

---
+++ welchcronin-discussion1.1.js
@@ -0,0 +1,19 @@
+
+/*
+============================================
+; Title:  Discussion 1.1
+; Author: Georgia Welch Cronin and ideas from Richard Krasso 
+; Modified by: Georgia Welch Cronin
+; Date:  21 October 2020
+; Description: Creating a simple Javascript example that uses values and variables and has at three small errors that can be fixed.
+;===========================================
+*/
+
+//a list of things people like to do
+let firstActivity = 'running,';
+let secondActivity = 'cycling,';
+let thirdActivity = 'hiking,';
+let fourthActivity = 'camping.';
+
+//the output of the list above
+console.log('My favorite activities are' + firstactivity + '' secondActivity + ' ' +  thirdActivity + ' and ' +  fourthActivity );
