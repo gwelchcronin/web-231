07c18aea9c573254df7dd455fce157d99aa4bc2a
Georgia Welch Cronin <georgia@braiid.com>
2020-11-03 11:35:16 -0600

    adding 3.1 and 3.2 to repo

---
+++ week 3/welchcronin-assignment 3.1.js
@@ -0,0 +1,21 @@
+/*
+============================================
+; Title:  welchcronin-assignment3.1.js
+; Author: Professor Krasso 
+; Date:   03 November 2020
+; Modified By: Georgia Welch Cronin
+; Additional code loop ideas from: https://www.javascripttutorial.net/javascript-while-loop/
+; Description: Creating a control statement with 2 errors
+;===========================================
+*/
+
+const header = require('./welchcronin-header.js');
+
+console.log(header.display("Georgia", "WelchCronin", "Assignment 3.1"));
+
+//start program
+
+let count = 0; //here the count starts at 0 before the loop begins
+while (count < 15)  //this is where the while is checked before the loop runs, if the number remains below the threshold set to 15 it will continue
+    console.log(conut); //this is the calling of the function, telling the loop to run and to add 3 to each iteration of the count until we reach 15
+    count +=3;
---
+++ week 3/welchcronin-exercise3.2.js
@@ -0,0 +1,72 @@
+/*
+============================================
+; Title:  welchcronin-assignment3.2.js
+; Author: Professor Krasso 
+; Date:   03 November 2020
+; Modified By: Georgia Welch Cronin
+; Description: Pattern Matching Functions
+;===========================================
+*/
+
+const header = require('./welchcronin-header.js');
+
+console.log(header.display("Georgia", "WelchCronin", "Assignment 3.1"));
+
+//start program
+
+
+//local variables are set for this test
+let testVar1 = "Truck";
+let testVar2 = "Car"
+let testVar3 = "Camper"
+let testVar4 = "Camper"
+let testVar5 = "Bicycle"
+let testVar6 = "Tricycle"
+
+
+//first function is defined showing if arg1 and arg2 are absolutely equal
+function match(arg1, arg2){   
+    if (arg1 === arg2)
+        return true
+    else
+        return false
+} 
+
+//second function is defined with 2 parameters, arg1 and arg2
+//the return for this function shows when arg1 and arg2 parameters are not equal
+function logMismatch(arg1, arg2) { 
+    console.log(arg1 + " and " + arg2 + " do not match!");
+}
+
+//third function is defined showing two parameters, arg1 and arg2
+//the return for this function shows when arg1 and arg2 are equal
+function logMatch(arg1, arg2) {
+    console.log(arg1 + " and " + arg2 + " do match!");
+}
+
+
+//output from the match() function
+console.log('\n'); //adding a line break between header and exercise output
+console.log(match("A", "B"));
+console.log(match(2,2));
+
+//if else statement to test the functions with variables 1 & 2
+if (match(testVar1, testVar2)) {
+    logMatch(testVar1, testVar2);
+} else {
+    logMismatch(testVar1, testVar2);
+}
+
+//if else statement to test the functions with variables 3 & 4
+if (match(testVar3, testVar4)) {
+    logMatch(testVar3, testVar4);
+} else {
+    logMismatch(testVar3, testVar4);
+}
+
+//if else statement to test the functions with variables 5 & 6
+if (match(testVar5, testVar6)) {
+    logMatch(testVar5, testVar6);
+} else {
+    logMismatch(testVar5, testVar6);
+}
