22fe0c4091ef6a4e9ffc2d506073748723af8d91
Georgia Welch Cronin <georgia@braiid.com>
2020-11-07 12:30:13 -0600

    adding assignment 3.4 to repo

---
+++ week 3/welchcronin-assignment3.4.js
@@ -0,0 +1,59 @@
+/*
+============================================
+; Title:  welchcronin-assignment3.4.js
+; Author: Professor Krasso 
+; Date:   07 November 2020
+; Modified By: Georgia Welch Cronin
+; Description: Loops
+;===========================================
+*/
+
+const header = require('./welchcronin-header.js');
+
+console.log(header.display("Georgia", "WelchCronin", "Assignment 3.4"));
+console.log('\n'); //Here I am adding a line break between header and exercise output.
+console.log("---DO THE NUMBERS MATCH GAME---")
+
+//start program
+
+
+//This is a function supplied that will provide a random number between 1 and 10 for the assignment.
+function randomNumber() {
+    return Math.floor((Math.random() * 10) + 1)
+  }
+
+  //This is creating a variable (testNumber) with a value between 1 and 10 (8), and creating a For loop with 10 iterations (<10) of a local variable (x) that is assigned the random number,
+//Here I am creating an if else statement to test if the variable (testNumber) is equal to the randomNumber (x).
+
+let testNumber = 3;
+for (var x = 0; x < 10; x++) {
+    x = randomNumber()
+    if (match(testNumber, x)) {
+        logMatch(testNumber, x);
+    } else {
+        logMismatch(testNumber, x);
+    }    
+
+}
+//Here I am copying functions from 3.2 for match, logMismatch and logMatch.
+//The first function is defined and shows if arg1 and arg2 are absolutely equal or if they are not.
+function match(testNumber, randomNumber){   
+    if (testNumber === randomNumber)
+        return true
+    else
+        return false
+} 
+
+//The logMismatch function is defined with 2 parameters, arg1 and arg2.
+//The return for this function shows when arg1 and arg2 parameters are not equal.
+function logMismatch(arg1, arg2) { 
+    console.log(arg1 + " does not match " + arg2 + " !");
+}
+
+//The logMatch function is defined showing two parameters, arg1 and arg2.
+//The return for this function shows when arg1 and arg2 are equal.
+function logMatch(arg1, arg2) {
+    console.log(arg1 + " does match " + arg2 + " !");
+}
+
+
